06eb1a59d68645d5b8d9ac8470cf1286e064031e
brianhkang1@gmail.com <brianhkang1@gmail.com>
2018-05-23 22:51:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function shout(string) {
 return string.toUpperCase()
 }
 
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
 
 function logShout(string) {
   console.log(string.toUpperCase())
@@ -13,4 +15,9 @@ function logShout(string) {
 
 function logWhisper(string) {
   console.log(string.toLowerCase())
+}
+
+
+function sayHiToGrandma(string) {
+  if 
 }
